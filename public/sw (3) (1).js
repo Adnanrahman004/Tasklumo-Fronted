57fbe323ed3a7e3d6b187c36565b03561fbae671
Adnan rahman <rahmanadnan0551@gmail.com>
2026-08-13 16:04:55 +0530

    Add sw filr

--- public/sw (3) (1).js
+++ public/sw (3) (1).js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11567347
+    "zoneId": 11567575
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
